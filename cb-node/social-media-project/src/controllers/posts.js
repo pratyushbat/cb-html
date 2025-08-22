74360348ff26616c70baa6835042353617e77052
Pratyush Kumar <pratyush3030gmail.compratyush3030gmail.com>
2025-08-22 12:27:46 +0530

    create post show post

---
+++ cb-node/social-media-project/src/controllers/posts.js
@@ -0,0 +1,44 @@
+const { Posts, Users } = require("../db/model");
+
+async function createNewPost(userId, title, body) {
+  const post = Posts.create({
+    title,
+    body,
+    userId,
+  });
+  return post;
+}
+async function showAllPost(query) {
+  const posts = await Posts.findAll({
+    include: [Users],
+  });
+  return posts;
+}
+
+// Test Code
+// async function task() {
+  //   console.log(
+  //     await createNewPost(
+  //       1,
+  //       "My cat",
+  //       "my puppy cat is very good.he help in all timee and loyal to me"
+  //     )
+  //   );
+  //   console.log(
+  //     await createNewPost(
+  //       2,
+  //       "My dog",
+  //       "my puppy dog is very good.he help in all timee and loyal to me"
+  //     )
+  //   );
+
+
+  //   const posts = await showAllPost();
+//   for (let p of posts) {
+//     console.log(`${p.title} \nauthor: ${p.user.username} \n${p.body} \n`);
+//   }
+// }
+// task();
+
+
+module.exports = { createNewPost ,showAllPost };
